836873b62af6281e8833ce1e78ce5d69ccbe7482
Wilker Aziz <wilker.aziz@gmail.com>
2012-11-16 17:15:11 +0100

    A simple tool to perform all substrings matching

--- examples/matching.py
+++ examples/matching.py
@@ -2,45 +2,61 @@ from pyfst.algorithm.matching import substring_matcher, trie_matcher
 from pyfst.algorithm.util import path_fsa, draw
 from pytrie import Trie
 from time import time
+import sys
 
+if len(sys.argv) < 5:
+    print >> sys.stderr, 'Usage: python %s substring|trie outstem vocab [pattern/weight]+ < queries' % sys.argv[0]
+    print >> sys.stderr, 'Examples: echo "4 5 1 2 3 6" | python %s substring matching-substring 1-10 1,2,3/2' % sys.argv[0]
+    print >> sys.stderr, 'Examples: echo "5 3 1 2 3 4" | python %s trie matching-trie 1-10 1,2,3/2 1,2/1 3,1,2,3/5' % sys.argv[0]
+    sys.exit(0)
 
+alg = sys.argv[1]
+ostem = sys.argv[2]
+lower, upper = sys.argv[3].split('-')
+lower, upper = int(lower), int(upper)
+patterns = sys.argv[4:]
+V = range(lower, upper + 1)
 
-dfaSM1 = substring_matcher(range(1,4), [1,2,1,2], 10) 
-draw(dfaSM1, 'matching-substring1')
-print 'matching-substring1'
+if alg == "substring":
+    if len(patterns) != 1:
+        print sys.stderr, "I'm using only the first pattern"
+    pattern, weight = patterns[0].split('/')
+    print 'pattern: %s (%s)' % (pattern, weight)
+    pattern = [int(x) for x in pattern.split(',')]
+    weight = float(weight)
+    dfa = substring_matcher(V, pattern, weight)
+    draw(dfa, ostem)
+    try:
+        while True:
+            query = [int(x) for x in raw_input().split()]
+            f = path_fsa(query)
+            f = dfa.intersect(f)
+            for path in f.paths():
+                arcs = [(arc.ilabel, float(arc.weight)) for arc in path]
+                print '%s: %f' % (' '.join(['%d:%s' % (l, str(w)) for l, w in arcs]), sum(w for l, w in arcs))
+    except EOFError:
+        sys.exit(0)
+        
+elif alg == "trie":
+    trie = Trie()
+    for pattern in patterns:
+        pattern, weight = pattern.split('/')
+        print '+pattern: %s (%s)' % (pattern, weight)
+        pattern = tuple(int(x) for x in pattern.split(','))
+        weight = float(weight)
+        trie[pattern] = weight
+    dfa = trie_matcher(V, trie)
+    draw(dfa, ostem)
+    try:
+        while True:
+            query = [int(x) for x in raw_input().split()]
+            f = path_fsa(query)
+            f = dfa.intersect(f)
+            for path in f.paths():
+                arcs = [(arc.ilabel, float(arc.weight)) for arc in path]
+                print '%s: %f' % (' '.join(['%d:%s' % (l, str(w)) for l, w in arcs]), sum(w for l, w in arcs))
+    except EOFError:
+        sys.exit(0)
+else:
+    print >> sys.stderr, "Unknown algorithm", alg
 
-dfaSM2 = substring_matcher({'the':[1,2], 'black':[3,4], 'dog':[5,6], 'barked':[7,8]}, ['the', 'black', 'the'], 10) 
-draw(dfaSM2, 'matching-substring2')
-print 'mathching-substring2'
-
-N1 = Trie()
-N1[tuple([2,3])] = 1
-N1[tuple([1,2,3])] = 2
-N1[tuple([2,3,4])] = 3
-N1[tuple([1,2,3,4])] = 4
-N1[tuple([4,1])] = 5
-N1[tuple([1,2])] = 0.5
-V = range(1,200000)
-t0 = time()
-dfaTM1 = trie_matcher(V, N1)
-t1 = time()
-print 'trie matcher', t1-t0
-p1 = [1,2,3,4,1,2,3,4]
-draw(dfaTM1.intersect(path_fsa(p1)), 'matching-trie-path1')
-
-N2 = Trie()
-N2[tuple('a b'.split())] = 1
-N2[tuple('b c'.split())] = 1.5
-N2[tuple('b c d'.split())] = 2
-N2[tuple('e a b d'.split())] = 3
-N2[tuple('a b c d e'.split())] = 4
-V2 = {'a':[1], 'b':[2], 'c':[3], 'd':[4], 'e':[5], 'f':[6]}
-t0 = time()
-dfaTM2 = trie_matcher(V2, N2)
-t1 = time()
-print 'trie matcher', t1-t0
-draw(dfaTM2, 'matching-trie-regex')
-p2 = [1,2,3,4]
-draw(dfaTM2.intersect(path_fsa(p2)), 'matching-trie-path2')
-p3 = [5,1,2]
-draw(dfaTM2.intersect(path_fsa(p3)), 'matching-trie-path3')
