8521dbe012cc24ccc4c0be6792524d35aa16796e
Wilker Aziz <wilker.aziz@gmail.com>
2012-11-16 13:16:43 +0100

    added --mustache to setup.py

--- README.md
+++ README.md
@@ -10,9 +10,10 @@ Python interface to [OpenFst](http://openfst.org)
 
 3. Or you can do
 
-        python setup.py build_ext -i [--cython]
+        python setup.py build_ext -i [--mustache] [--cython]
 
-    If you use the option `--cython` the setup will compile pyx files into cpp files (requires Cython 0.17.1)
+    If you use the option `--mustache` the mustache templates will be used to generate `fst.pyx` and `cfst.pxd` (requires [mustache](http://mustache.github.com/)).
+    If you use the option `--cython` the setup will compile pyx files into cpp files (requires [Cython 0.17.1](http://cython.org)).
 
 ## Usage
 
@@ -51,5 +52,11 @@ t[2].final = 3.5
 
 t.shortest_path() # 2 -(a:A/0.5)-> 1 -(c:C/2.5)-> 0/3.5 
 ```
+
+## Examples
+
+    In `examples` you will find a bunch of test cases, e.g. `edit.py`, `sampling.py`, etc.
+
+## IPython notebook
 
 The pyfst API is [IPython notebook](http://ipython.org/ipython-doc/dev/interactive/htmlnotebook.html)-friendly: the transducers objects are [automatically drawn](http://nbviewer.ipython.org/3835477/) using [Graphviz](http://www.graphviz.org).
--- setup.py
+++ setup.py
@@ -1,20 +1,39 @@
 from distutils.core import setup
 from distutils.extension import Extension
 import sys
+import os
 
-suffix = '.cpp'
-cmdcls = {}
+ymls = ['pyfst/types.yml']
+tpls = ['pyfst/cfst.pxd.tpl', 'pyfst/fst.pyx.tpl']
+
+# treats the switch --mustache
+try:
+    sys.argv.remove('--mustache')
+    if os.system('mustache -v') != 0:
+        raise OSError, "You don't seem to have `mustache` installed"
+    types = ' '.join(ymls)
+    for tpl in tpls:
+        src = tpl[:-4] # removes .tpl
+        cmd = 'cat %s %s | mustache > %s' % (types, tpl, src)
+        if os.system(cmd) != 0:
+            raise OSError, "Problem running: %s" % cmd
+        else: 
+            print cmd, '[success]'
+except ValueError:
+    print 'Skipping mustaching'
+
+# treats the switch --cython
+cythoning = False
 try:
     sys.argv.remove('--cython')
     from Cython.Distutils import build_ext
-    suffix = '.pyx'
-    cmdcls['build_ext'] = build_ext
+    cythoning = True
 except ValueError:
     print 'Skipping cythoning'
 except ImportError:
     print 'You do not seem to have Cython installed'
 
-
+suffix = '.pyx' if cythoning else '.cpp'
 extensions = [
             Extension(name='pyfst.fst',
                 sources=['pyfst/fst' + suffix],
@@ -44,7 +63,7 @@ extensions = [
             ]
 
 setup(
-    cmdclass = cmdcls,
+    cmdclass = {'build_ext':build_ext} if cythoning else {},
     name = 'pyfst',
     ext_modules= extensions,
     packages = ['pyfst'],
